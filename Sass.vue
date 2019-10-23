7f4cd42cd0e46607eb0bf524be5d13e9417933ad
Andrew Koidan <deathangel908@gmail.com>
2019-10-23 20:57:36 +0300

    first

---
+++ Sass.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+<script lang="ts">
+
+  export default {}
+</script>
+<style lang="sass" scoped>
+
+  $class-slug: for !default
+
+  @for $i from 1 through 4
+    .#{$class-slug}-#{$i}
+      width: 60px + $i
+      color: black
+
+
+</style>
+
+
---
+++ Scss.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  </div>
+</template>
+<script lang="ts">
+
+  export default {}
+</script>
+<style lang="scss" scoped>
+
+  $class-slug: for !default
+
+  @for $i from 1 through 4 {
+    .#{$class-slug}-#{$i} {
+      width: 60px + $i;
+      color: black;
+    }
+  }
+
+
+</style>
+
+
